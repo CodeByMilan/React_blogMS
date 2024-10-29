d10a599473a4142129448940c059be71a72c76db
Milan Acharya <milanacharya1614@gmail.com>
2024-10-29 15:20:31 +0545

    completd ui

---
+++ src/pages/SingleBolg.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+
+const SingleBolg = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div
+  className="flex flex-col rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white md:flex-row w-screen h-screen p-8 md:p-12 space-x-4">
+  <img
+    className="h-[80%] w-2/5 rounded-t-lg object-cover md:!rounded-none md:!rounded-s-lg"
+    src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+    alt="Card image" />
+  <div className="flex flex-col justify-start p-3 pt-0 w-3/5">
+    <h5 className="mb-2 text-5xl font-bold">Blog Title</h5>
+    <p className="mb-4 text-lg">Category</p>
+    <p className="mb-4 text-lg">
+      This is a wider card with supporting text below as a natural lead-in
+      to additional content. This content is a little bit longer.
+    </p>
+    <p className="text-sm text-surface/75 dark:text-neutral-300">
+      Published on 2022-01-01
+    </p>
+    <p className="text-sm pt-5 mt-2 text-surface/75 font-medium font-serif text-xl dark:text-neutral-300">
+      By Milan Acharya
+    </p>
+    <div className="flex  mt-4">
+      <Link to="/blog/edit">
+        <button className="bg-black text-white px-10 py-3 mx-5 rounded-md focus:outline-none">Edit</button>
+      </Link>
+      <Link to="/blog/delete">
+        <button className="bg-black text-white px-10 py-3 mx-5 rounded-md focus:outline-none">Delete</button>
+      </Link>
+    </div>
+  </div>
+</div>
+
+    </>
+  );
+};
+
+export default SingleBolg;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,53 +1,14 @@
-import React from 'react';
+import React from "react";
+import Form from "./components/Form";
+import { Navbar } from "../../components/Navbar";
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center h-screen"> 
-      <div className='shadow-md p-10 w-[60%] max-w-md'> 
-        <h1 className='text-center font-serif font-bold text-3xl mb-10'>Login</h1>
-        <form className="max-w-sm mx-auto">
-          <div className="mb-5">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-            <input
-              type="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="yourname@gmail.com"
-              required
-            />
-          </div>
-          <div className="mb-5">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
-            <input
-              type="password"
-              id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              required
-            />
-          </div>
-          <div className="flex items-start mb-5">
-            <div className="flex items-center h-5">
-              <input
-                id="remember"
-                type="checkbox"
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
-                required
-              />
-            </div>
-            <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900">Remember me</label>
-          </div>
-          <div className="flex justify-center"> {/* Centering the button */}
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <>
+    <Navbar/>
+      <Form type="Login"/>
+    </>
   );
-}
+};
 
 export default Login;
